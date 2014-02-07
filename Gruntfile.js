4bfcdd9d0e711c2f7109e2779dce149207bd205d
Thomas Stradling <protolich@gmail.com>
2014-02-07 21:35:51 +0000

    Added package.json, Gruntfile and build folder

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			build: {
+				files: {
+					'build/jquery.swishCarousel.min.js': ['src/jquery.swishCarousel.js']
+				}
+			}
+		},
+		jshint: {
+			src: ['src/jquery.swishCarousel.js'],
+			build: ['build/jquery.swishCarousel.min.js']
+		}
+	});
+
+	// Load uglify
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	// Load jshint
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+
+	grunt.registerTask('default', ['jshint:src','uglify','jshint:build']);
+};
